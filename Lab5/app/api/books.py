23ebab5483246e88ff13cb93567e4df20ca05b9b
KroshneiRostyslav <rostyslav.kroshnei.23@pnu.edu.ua>
2026-06-25 03:12:02 +0300

    Lab 5 complited

--- Lab5/app/api/books.py
+++ Lab5/app/api/books.py
@@ -8,116 +8,135 @@ service = get_book_service()
 
 class BookListResource(Resource):
 
-    def get(self):
-        """
-        Get all books
-        ---
-        tags:
-          - Books
+  def get(self):
+    """
+    Get all books
+    ---
+    tags:
+      - Books
 
-        parameters:
-          - name: limit
-            in: query
-            type: integer
+    parameters:
+      - name: limit
+        in: query
+        type: integer
 
-          - name: offset
-            in: query
-            type: integer
+      - name: offset
+        in: query
+        type: integer
 
-        responses:
-          200:
-            description: Books list
-        """
+    responses:
+      200:
+        description: Books list
+    """
 
-        limit = int(
-            request.args.get("limit", 10)
-        )
+    limit = int(
+        request.args.get("limit", 10)
+    )
 
-        offset = int(
-            request.args.get("offset", 0)
-        )
+    offset = int(
+        request.args.get("offset", 0)
+    )
 
-        result = service.get_all_books(
-            limit,
-            offset
-        )
+    result = service.get_all_books(
+        limit,
+        offset
+    )
 
-        return result, 200
+    return result, 200
 
-    def post(self):
-        """
-        Create book
-        ---
-        tags:
-          - Books
+  def post(self):
+    """
+    Create book
+    ---
+    tags:
+      - Books
 
-        parameters:
-          - in: body
-            name: body
+    parameters:
+      - in: body
+        name: body
+        required: true
+        schema:
+          type: object
+          properties:
+            title:
+              type: string
+              example: Test name
 
-        responses:
-          201:
-            description: Book created
-        """
+            author:
+              type: string
+              example: Super author
 
-        data = request.get_json()
+            year:
+              type: integer
+              example: 2020
 
-        result = service.add_book(data)
+          required:
+            - title
+            - author
 
-        return result, 201
+    responses:
+      201:
+        description: Book created
+    """
+
+    data = request.get_json()
+
+    result = service.add_book(data)
+
+    return result, 201
 
 
 class BookResource(Resource):
 
-    def get(self, book_id):
-        """
-        Get book by id
-        ---
-        tags:
-          - Books
+  def get(self, book_id):
+      """
+      Get book by id
+      ---
+      tags:
+        - Books
 
-        parameters:
-          - name: book_id
-            in: path
-            required: true
+      parameters:
+        - name: book_id
+          in: path
+          required: true
 
-        responses:
-          200:
-            description: Book
-          404:
-            description: Not found
-        """
+      responses:
+        200:
+          description: Book
+        404:
+          description: Not found
+      """
 
-        book = service.get_book_by_id(
-            book_id
-        )
+      book = service.get_book_by_id(
+          book_id
+      )
 
-        if not book:
-            return {
-                "message": "Book not found"
-            }, 404
+      if not book:
+          return {
+              "message": "Book not found"
+          }, 404
 
-        return book, 200
+      return book, 200
 
-    def delete(self, book_id):
-        """
-        Delete book
-        ---
-        tags:
-          - Books
+  def delete(self, book_id):
+      """
+      Delete book
+      ---
+      tags:
+        - Books
 
-        parameters:
-          - name: book_id
-            in: path
-            required: true
+      parameters:
+        - name: book_id
+          in: path
+          required: true
 
-        responses:
-          204:
-            description: Deleted
-        """
+      responses:
+        204:
+          description: Deleted
+      """
 
-        service.delete_book_by_id(
-            book_id
-        )
+      service.delete_book_by_id(
+          book_id
+      )
 
-        return "", 204
+      return "", 204
